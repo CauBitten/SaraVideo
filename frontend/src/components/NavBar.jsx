b8666474cacd480c4a6a9b0324c37c5a85df62ee
Higor Cordeiro <higormatheus1618@gmail.com>
2024-08-28 19:48:38 -0300

    Clicar em Repositories redireciona para a página de repositórios.

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Layout, Menu, Input, Button } from 'antd';
-import { SearchOutlined, LogoutOutlined } from '@ant-design/icons';
+import { Layout, Menu, Button } from 'antd';
+import { LogoutOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 
 const { Header } = Layout;
 
 const NavBar = () => {
+  const navigate = useNavigate(); // Hook para navegação
+
+  const handleRepositoriesClick = () => {
+    navigate('/'); // Redireciona para a página inicial
+  };
+
   return (
     <Header className="custom-header">
       <Menu theme="dark" mode="horizontal" className="custom-menu">
         <Menu.Item key="1" className="custom-menu-item">Overview</Menu.Item>
-        <Menu.Item key="2" className="custom-menu-item">Repositories</Menu.Item>
+        <Menu.Item key="2" className="custom-menu-item" onClick={handleRepositoriesClick}>
+          Repositories
+        </Menu.Item>
       </Menu>
       <div className="custom-logout-container">
         <Button type="default" icon={<LogoutOutlined />} className="custom-logout-button">Logout</Button>
