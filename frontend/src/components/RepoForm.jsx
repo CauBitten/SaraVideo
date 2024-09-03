16b9dd90872a3a50b4e7a2729d93b05108678e62
Anthony <anthony_efraim@hotmail.com>
2024-09-02 23:30:47 -0300

    criar-repo finalizada

--- frontend/src/components/RepoForm.jsx
+++ frontend/src/components/RepoForm.jsx
@@ -31,26 +31,29 @@ function RepoForm({ route }) {
       <div id="repository-form">
         <h1>Create a new repository</h1>
         <h4>A repository contains all videos, including the analytics.</h4>
-        <div className="form-box">
-          <label>Owner</label>
-          <input
-            className="form-input repo-form-input"
-            type="text"
-            /* value={usuario} readOnly  favor editar*/
-            style={{ width: "130px" }}
-          />
+        <div id="owner-name">
+          <div className="form-box">
+            <label>Owner</label>
+            <input
+              className="form-input repo-form-input"
+              type="text"
+              /* value={usuario} readOnly  favor editar*/
+              style={{ width: "130px" }}
+            />
+          </div>
+          <span id="barra">/</span>
+          <div className="form-box">
+            <label>Repository name</label>
+            <input
+              className="form-input repo-form-input"
+              type="text"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              style={{ width: "246px" }}
+            />
+          </div>
         </div>
-        <div className="form-box">
-          <label>Repository name</label>
-          <input
-            className="form-input repo-form-input"
-            type="text"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            style={{ width: "246px" }}
-          />
-        </div>
-        <div className="form-box">
+        <div id="form-description" className="form-box">
           <label>Description</label>
           <input
             className="form-input repo-form-input"
@@ -59,16 +62,48 @@ function RepoForm({ route }) {
             onChange={(e) => setDescricao(e.target.value)}
             style={{ width: "420px" }}
           />
-          <div className="form-checkbox">
-            <input type="checkbox" />
-            <label>Private Repository?</label>
-          </div>
         </div>
+        <div className="form-checkbox">
+          <input type="checkbox" />
+          <label>Private Repository?</label>
+        </div>
+
+        <div className="form-dropdown">
+          <label>Collaborators</label>
+          <select id="options" name="options">
+            <option value="option1">Collaborator 1</option>
+            <option value="option2">Collaborator 2</option>
+            <option value="option3">Collaborator 3</option>
+          </select>
+        </div>
+
+        <div id="personal-alert">
+          <span id="alert-i">i</span>
+          You are creating a repository in your personal account.
+        </div>
+
         <button className="form-button" type="submit">
           Create Repository
         </button>
       </div>
-      <div id="repository-image">htrhtrhb</div>
+      <div id="repository-image">
+        <label>Repository Image</label>
+        <div
+          id="repository-image-caixa"
+          className="file-upload-box"
+          onClick={() => document.getElementById("file-input").click()}
+        >
+          +
+        </div>
+
+        <input
+          id="file-input"
+          type="file"
+          accept="image/*"
+          style={{ display: "none" }}
+          /* onChange={(e) => handleFileUpload(e)} */
+        />
+      </div>
     </form>
   );
 }
