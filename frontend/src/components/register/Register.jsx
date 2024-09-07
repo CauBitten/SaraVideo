b75d96d89a297ccadd2e9f141afd85f47a1db8a9
Anthony <anthony_efraim@hotmail.com>
2024-09-07 10:55:50 -0300

    small fix: melhorando comportamento da imagem no login/registro

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -1,18 +1,25 @@
-import RegisterForm from "./RegisterForm"
+import RegisterForm from "./RegisterForm";
 import HomeImage from "../../static/home.jpeg";
 
 function Register() {
-    return <div className="container">
-        <h1 className="title">SaraVideo Manager</h1>
-        <div className="row">
-            <div className="col container-form">
-                <RegisterForm route="/api/user/register/" />
-            </div>
-            <div className="col">
-                <img src={HomeImage} alt="Example" className="image" />
-            </div>
+  return (
+    <div className="container">
+      <h1 className="title">SaraVideo Manager</h1>
+      <div className="row">
+        <div className="col container-form">
+          <RegisterForm route="/api/user/register/" />
         </div>
+        <div className="col">
+          <img
+            src={HomeImage}
+            alt="Example"
+            className="image"
+            style={{ height: "120%" }}
+          />
+        </div>
+      </div>
     </div>
+  );
 }
 
-export default Register
+export default Register;
